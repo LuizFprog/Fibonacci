3f07dc5b2865760be2a2d2381af546d0cb7b7f67
LuizFprog <luizgomes.prog@gmail.com>
2024-10-15 07:16:46 -0300

    fix: correção na mensagem

--- src/fibonacci/Main.java
+++ src/fibonacci/Main.java
@@ -6,9 +6,18 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int number;
+		int[] fibonacci = {0, 1, 2, 3, 5, 8, 13}; // Fibonacci removendo os repetidos para verificação
 		System.out.println("Digite um número: ");
 		number = sc.nextInt();
-		System.out.println("Fibonacci de " + number + " = " + fibonacci(number));
+		int fib = fibonacci(number);
+		
+		System.out.println("Fibonacci de " + number + " = " + fib);
+		
+		for(int i = 0; i < fibonacci.length; i++) {
+			if(fib == fibonacci[i]) {
+				System.out.println("Para o Fibonacci de até 7, o valor está na sequência");
+			}
+		}
 	}
 	
 	public static int fibonacci(int number) {
